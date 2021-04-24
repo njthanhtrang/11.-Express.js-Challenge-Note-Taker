d6f7e397d0269acdbcdfb4c403e51de69f09b5ca
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-24 13:06:39 -0700

    added routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,14 +4,17 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require("fs");
 const path = require("path");
 
+// GET /api/notes
 router.get("/notes", (req, res) => {
   console.log({data});
   res.json(data);
 });
 
+// DELETE /api/notes/:id
 // get one specific note
 // req.body.id, readfile db.json, find entry that matches that id
 router.delete("/notes/:id", (req, res) => {
+    // rewrite data and return only elements that DON'T match deleted note ID
     data = data.filter(el => el.id !== req.params.id);
     fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(data), function(err) {
         if (err) {
@@ -23,6 +26,7 @@ router.delete("/notes/:id", (req, res) => {
 
 //   create new UUID, take note out of req.body, apply UUID, save to db.json
 router.post("/notes", (req, res) => {
+    // spread operator
     const newNote = {...req.body, id: uuidv4()};
     console.log(newNote);
     console.log(req.body);
