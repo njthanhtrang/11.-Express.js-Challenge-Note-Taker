7836fa9914e4a89c3185b9bb32dbf909cb7c1c24
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-24 13:35:54 -0700

    connect to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
@@ -11,6 +12,4 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(3001, () => {
-    console.log("API server now on port 3001!");
-}) ;
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
